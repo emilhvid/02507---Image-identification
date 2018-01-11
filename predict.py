878518b9a05f8cfbe54627afabb1cd5a91a1a0b0
emilhvid <hvid2301@gmail.com>
2018-01-11 10:56:39 +0100

    prediction relative paths and 6 classes

--- predict.py
+++ predict.py
@@ -5,9 +5,10 @@ import sys,argparse
 
 
 # First, pass the path of the image
-dir_path = os.path.dirname(os.path.realpath(__file__))
-image_path='testing_data\\dogs\\dog.1001.jpg'
+dir_path = os.path.dirname(os.path.realpath(__file__))		  
+image_path=sys.argv[1]
 filename = dir_path +'/' +image_path
+
 image_size=128
 num_channels=3
 images = []
@@ -25,10 +26,9 @@ x_batch = images.reshape(1, image_size,image_size,num_channels)
 ## Let us restore the saved model 
 sess = tf.Session()
 # Step-1: Recreate the network graph. At this step only graph is created.
-saver = tf.train.import_meta_graph('dogs-cats-model.meta')
+saver = tf.train.import_meta_graph(dir_path+'/model/dogs-cats-model.meta')
 # Step-2: Now let's load the weights saved using the restore method.
-saver.restore(sess, tf.train.latest_checkpoint('./'))
-
+saver.restore(sess, tf.train.latest_checkpoint(dir_path+'/model/'))
 # Accessing the default graph which we have restored
 graph = tf.get_default_graph()
 
@@ -39,14 +39,14 @@ y_pred = graph.get_tensor_by_name("y_pred:0")
 ## Let's feed the images to the input placeholders
 x= graph.get_tensor_by_name("x:0") 
 y_true = graph.get_tensor_by_name("y_true:0") 
-y_test_images = np.zeros((1, 2)) 
-
+y_test_images = np.zeros((1, 6)) 
 
 ### Creating the feed_dict that is required to be fed to calculate y_pred 
 feed_dict_testing = {x: x_batch, y_true: y_test_images}
 result=sess.run(y_pred, feed_dict=feed_dict_testing)
+print(result)
 # result is of this format [probabiliy_of_rose probability_of_sunflower]
-TRAIN_DIR='C:/Users/Adrien/AppData/Local/Programs/Python/Python36/cv-tricks.com-master/Tensorflow-tutorials/tutorial-2-image-classifier/training_data'
+TRAIN_DIR=dir_path+'/training_data'
 classes = os.listdir(TRAIN_DIR)
 num_classes = len(classes)
 res=[]
